96deeac6782029aec5658dc922da128b44424b36
rahmanabdulquadir <rahmanaq777@gmail.comm>
2022-09-04 11:35:20 +0600

    all section done

--- js/app.js
+++ js/app.js
@@ -2,5 +2,4 @@ const loadNews = () => {
   const url = `https://openapi.programming-hero.com/api/news/category/01`
   fetch(url)
   .then(res => res.json())
-  .then(data => displayNews(data.data))
-}
+  
